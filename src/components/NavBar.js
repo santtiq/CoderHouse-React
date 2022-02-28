543d1ef2fa70c374299b3ef5d743fb6bf8b99bb1
santtiq <santiquinteros@frba.utn.edu.ar>
2022-02-27 22:12:12 -0300

    new navbar, challenge 1

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function Header() {
+    return <header class="nav-container">
+        <h1>Brand</h1>
+        <nav>
+            <a href="#" class="nav-link">Home</a>
+            <a href="#" class="nav-link">Shop</a>
+            <a href="#" class="nav-link">About</a>
+        </nav>
+    </header>
+}
+
+export default Header;
